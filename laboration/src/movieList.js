918b0736fc16117d120d806d95937df3c586a8a5
cwallins <Wallin1996@gmail.com>
2022-05-12 15:24:12 +0200

    template html

--- laboration/src/movieList.js
+++ laboration/src/movieList.js
@@ -5,6 +5,27 @@ export default function MovieList() {
         <div>
             <label for="title-field">Titel:</label>
             <input type="text" id="title-field" className="form-control"></input>
+            <lablel for="rating-field">Betyg:</lablel>
+
+            <select type="text" id="rating-field" class="form-control">
+                <option value="0">Välj betyg här...</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+                <option value="3">3</option>
+                <option value="4">4</option>
+                <option value="5">5</option>
+            </select>
+
+            <input type="submit" class="btn btn-success mt-3" value="Spara film"></input>
+
+            <hr></hr>
+
+            <h2>Filmer</h2>
+            <ul id="movies">
+                
+            </ul>
+
+
         </div>
     )
 }
